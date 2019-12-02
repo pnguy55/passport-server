bc3b0afeab3dac9ab336839bcc326d247f9e9701
Phi Nguyen <pnguy55@gmail.com>
2019-12-02 16:50:19 -0600

    Push to github

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -9,6 +9,6 @@ export const fetchUser = () => async dispatch => {
 
 export const handleToken = token => async dispatch => {
     const res = await axios.post('/api/stripe', token);
-    // this line dispatches an action that alerts the reducer
+    // this line dispatches an action that will alert the reducer
     dispatch({ type: FETCH_USER, payload: res.data });
 };
